1d78c4bb22c1616975715f4b691aa4d6a22ab9fe
KanesKaewchui <kaneskaewchui1@gmail.com>
2025-08-31 22:08:08 +0700

    Done

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { projects } from "./data/projects";
 import ProjectCard from "./components/ProjectCard";
