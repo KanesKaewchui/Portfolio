be5d58aa95ea05911803160784ccd82385e1c9d1
KanesKaewchui <kaneskaewchui1@gmail.com>
2025-09-04 11:29:43 +0700

    Edit layout

--- my-portfolio/src/app/layout.tsx
+++ my-portfolio/src/app/layout.tsx
@@ -5,18 +5,18 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portfolio | Your Name",
-  description: "Personal portfolio built with Next.js + Tailwind",
-  icons: { icon: "/favicon.ico" },
-  metadataBase: new URL("https://example.com"),
-  openGraph: {
-    title: "Portfolio | Your Name",
-    description: "Personal portfolio built with Next.js + Tailwind",
-    url: "https://example.com",
-    siteName: "Portfolio",
-    images: [{ url: "/profile.jpg", width: 1200, height: 630, alt: "Profile" }],
-    type: "website",
-  },
+  title: "Portfolio | Kanes Kaewchui",
+  // description: "Personal portfolio built with Next.js + Tailwind",
+  // icons: { icon: "/favicon.ico" },
+  // metadataBase: new URL("https://example.com"),
+  // openGraph: {
+  //   title: "Portfolio | Kanes Kaewchui",
+  //   description: "Personal portfolio built with Next.js + Tailwind",
+  //   url: "https://portfolio-zeta-brown-15.vercel.app/",
+  //   siteName: "Portfolio",
+  //   // images: [{ url: "/profile.jpg", width: 1200, height: 630, alt: "Profile" }],
+  //   type: "website",
+  // },
 };
 
 export default function RootLayout({
